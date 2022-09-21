fdd15aad4f5735b03ae87b73cbdacf4c7957124d
rameshkumars2 <rameshkumars2@accesshealthcare.com>
2022-09-21 20:39:04 +0530

    commit 1

--- static/js/load_500.js
+++ static/js/load_500.js
@@ -1,42 +1,54 @@
-function reload_500_data() {
-    $('#nifty_500').load('/bengaluru/load_five_hundred/');
+function reload_fh_data() {
+    $('#show_fh').load('/bengaluru/load_five_hundred/');
 }
 
-function reload_fh_zero_data() {
-    $('#fh_zero').load('/bengaluru/load_five_hundred_zero/');
+function reload_fhz_data() {
+    $('#show_fhz').load('/bengaluru/load_five_hundred_zero/');
 }
 
-$(document).ready(function(){
 
-    // Load table on page ready
-    reload_500_data()
-    reload_fh_zero_data()
-
-    // On button click load data
-    $(".load_500").click(function(){
-        reload_500_data()
-    });
-
-    $(".pull_500").click(function(){
-        $.get("/bengaluru/pull_five_hundred/");
-    });
-
-    // On button click load data
-    $(".load_fh_zero").click(function(){
-        reload_fh_zero_data()
-    });
-
-    // On button click load data
-    $(".get_zero").click(function(){
-        $.get("/bengaluru/get_zero_value/");
-    });
-
-});
+/* --------Start Auto reload--------- */
 
 // Data reload every 60 seconds
 setInterval(function(){
-    reload_500_data()
-    reload_fh_zero_data()
+    reload_fh_data()
+    reload_fhz_data()
 }, 1000 * 60);
 
+/* --------End Auto reload--------- */
 
+
+
+/* --------Start Button action--------- */
+
+// On button click load FH data
+$("#load_fh").click(function(){
+    reload_fh_data()
+});
+
+// On button click pull FH data
+$("#pull_fh").click(function(){
+    $.get("/bengaluru/pull_five_hundred/");
+});
+
+// On button click load FHZ data
+$("#load_fhz").click(function(){
+    reload_fhz_data()
+});
+
+// On button click analyse FHZ data
+$("#analyse_fhz").click(function(){
+    $.get("/bengaluru/get_zero_value/");
+});
+
+/* --------End Button action--------- */
+
+
+
+$(document).ready(function(){
+
+    // Load fh and fh zero table on page ready
+    reload_fh_data()
+    reload_fhz_data()
+
+});
