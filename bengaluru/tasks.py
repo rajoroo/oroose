012be07ddaa273ef51a8aa03492bf2b368137436
Madhavraj <madhavraj948@gmail.com>
2022-10-02 20:28:22 +0530

    commit 1

--- bengaluru/tasks.py
+++ bengaluru/tasks.py
@@ -3,14 +3,21 @@ from datetime import datetime
 from django.utils.timezone import get_current_timezone
 
 from core.configuration import ParameterStore
-from core.models import DataLog
+from core.models import DataLog, ParameterSettings
 from oroose.celery import app
 
 from .evaluation import analyse_stocks_five_hundred, polling_live_stocks_five_hundred
 
 
 LOG_SCHEDULE_LIVE_500 = ParameterStore().get_conf("LOG_SCHEDULE_LIVE_500")
-FH_LIVE_STOCKS_NSE = ParameterStore().get_conf("FH_LIVE_STOCKS_NSE")
+SETTINGS_FH_LIVE_STOCKS_NSE = "SETTINGS_FH_LIVE_STOCKS_NSE"
+
+
+def condition_schedule_live_stocks_fh():
+    ps = ParameterSettings.objects.get(name=SETTINGS_FH_LIVE_STOCKS_NSE)
+    if ps.status:
+        return True
+    return False
 
 
 @app.task
@@ -22,7 +29,8 @@ def schedule_live_stocks_five_hundred():
         name=LOG_SCHEDULE_LIVE_500,
     )
     obj.save()
-    if FH_LIVE_STOCKS_NSE:
+    print(condition_schedule_live_stocks_fh())
+    if condition_schedule_live_stocks_fh():
         print("Schedule live stocks five hundred in-progress")
         polling_live_stocks_five_hundred()
         print("Schedule live stocks five hundred zero in-progress")
