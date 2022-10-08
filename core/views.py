12845e4cd49ee29423cf2c4556d4b3741396fe96
Madhavraj <madhavraj948@gmail.com>
2022-10-08 12:36:11 +0530

    Commit 1

--- core/views.py
+++ core/views.py
@@ -5,17 +5,22 @@ from django.conf import settings
 
 
 def pre_check_server_start():
-    json_file = open(settings.LOAD_CONFIG_PATH)
-    config_data = json.load(json_file)
+    with open(settings.LOAD_CONFIG_PATH) as json_file:
+        config_data = json.load(json_file)
 
     confs = config_data["data"]
     conf = list(confs.keys())
 
     items = [
+        # API control
         "SETTINGS_FH_LIVE_STOCKS_NSE",
         "SETTINGS_FH_ZERO",
+
+        # API
         "LIVE_INDEX_URL",
         "LIVE_INDEX_500_URL",
+
+        # FHZ Configuration
         "FH_RANK_FROM",
         "FH_RANK_TILL",
         "FH_MAX_PRICE",
@@ -23,13 +28,19 @@ def pre_check_server_start():
         "FH_MAX_BUY_ORDER",
         "FH_STOCK_LIVE_START",
         "FH_STOCK_LIVE_END",
+
+        # Logger
         "LOG_SCHEDULE_LIVE_500",
         "LOG_SCHEDULE_ZERO_500",
+
+        # Timing
         "FH_ZERO_START",
         "FH_ZERO_END",
     ]
 
-    for item in items:
-        if item not in conf:
-            print("Please check config json file.")
-            sys.exit(1)
+    conf_not_exist = ', '.join([item for item in items if item not in conf])
+    if conf_not_exist:
+        print(f"Please check config json file keys - {conf_not_exist}")
+        sys.exit(1)
+
+    return True
