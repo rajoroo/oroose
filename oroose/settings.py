deaecde993275fe13771b9c9f69311c13f6be805
Madhavraj <madhavraj948@gmail.com>
2022-10-18 00:45:57 +0530

    commit 1

--- oroose/settings.py
+++ oroose/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = "oroose.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR, "templates"],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [BASE_DIR, "static"]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
