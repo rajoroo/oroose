37360e74e3a62e43ae9431f0602dd52950e8b0fb
maddy <madhavraj948@gmail.com>
2022-12-18 21:00:59 +0530

    commit 1

--- bengaluru/urls.py
+++ bengaluru/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path("fhz_uptrend_record/<int:pk>", views.EditFhzUptrend.as_view(), name="fhz_uptrend_record"),
-    path("load_fhz_uptrend_view/", views.load_fhz_uptrend_view, name="fhz_uptrend_view"),
-    path("load_fhz_uptrend_error_view/", views.load_fhz_uptrend_error_view, name="fhz_uptrend_error_view"),
-    path("load_fhz_uptrend_sold_view/", views.load_fhz_uptrend_sold_view, name="fhz_uptrend_sold_view"),
+    path("content/", views.load_bengaluru_content, name="bengaluru_content"),
     path("", views.bengaluru_page, name="bengaluru"),
 ]
--- bengaluru/views.py
+++ bengaluru/views.py
@@ -7,6 +7,10 @@ from django.shortcuts import reverse
 from bengaluru.models import FhZeroUpTrend, FhZeroStatus
 from django.views.generic.edit import UpdateView
 from django.shortcuts import render
+from django.db.models import Max
+from core.models import DataLog, ParameterSettings
+from stockwatch.models import FiveHundred
+from core.constant import SETTINGS_FH_LIVE_STOCKS_NSE, LOG_SCHEDULE_LIVE_500
 
 
 # Uptrend
@@ -16,46 +20,6 @@ def bengaluru_page(request):
     return render(request, "bengaluru/base_page.html", context)
 
 
-def load_fhz_uptrend_view(request):
-    """Load five hundred zero objects display in table view"""
-    fhz = (
-        FhZeroUpTrend.objects.filter(
-            date=datetime.today(),
-            status__in=[FhZeroStatus.TO_BUY, FhZeroStatus.PURCHASED, FhZeroStatus.TO_SELL],
-            error=False,
-        ).annotate(current_pl=F("quantity") * (F("current_price") - F("buy_price")))
-    ).order_by("-updated_date")
-
-    context = {
-        "items": list(fhz.values()),
-    }
-    return render(request, "bengaluru/load_fh_zero_view.html", context=context)
-
-
-def load_fhz_uptrend_error_view(request):
-    """Load five hundred zero objects display in table view"""
-    fhz = FhZeroUpTrend.objects.filter(
-        date=datetime.today(),
-        status__in=[FhZeroStatus.TO_BUY, FhZeroStatus.PURCHASED, FhZeroStatus.TO_SELL],
-        error=True,
-    ).annotate(profit_loss=F("quantity") * (F("sell_price") - F("buy_price")))
-
-    context = {
-        "items": list(fhz.values()),
-    }
-    return render(request, "bengaluru/load_fh_zero_error_view.html", context=context)
-
-
-def load_fhz_uptrend_sold_view(request):
-    """Load five hundred zero objects display in table view"""
-    fhz = FhZeroUpTrend.objects.filter(date=datetime.today(), status=FhZeroStatus.SOLD).annotate(
-        profit_loss=F("quantity") * (F("sell_price") - F("buy_price"))
-    )
-
-    context = {"items": list(fhz.values()), "realized_amount": fhz.aggregate(Sum("profit_loss"))["profit_loss__sum"]}
-    return render(request, "bengaluru/load_fh_zero_sold_view.html", context=context)
-
-
 class EditFhzUptrend(UpdateView):
     model = FhZeroUpTrend
     template_name = "trend_view/fhz_uptrend.html"
@@ -63,3 +27,39 @@ class EditFhzUptrend(UpdateView):
 
     def get_success_url(self):
         return reverse("fhz_uptrend_record", kwargs={"pk": self.object.id})
+
+
+def load_bengaluru_content(request):
+    live_500 = FiveHundred.objects.filter(date=datetime.today())
+    live_pull_on = DataLog.objects.filter(name=LOG_SCHEDULE_LIVE_500).aggregate(Max("end_time"))["end_time__max"]
+    ps = ParameterSettings.objects.get(name=SETTINGS_FH_LIVE_STOCKS_NSE)
+
+    progress = (
+        FhZeroUpTrend.objects.filter(
+            date=datetime.today(),
+            status__in=[FhZeroStatus.TO_BUY, FhZeroStatus.PURCHASED, FhZeroStatus.TO_SELL],
+            error=False,
+        ).annotate(current_pl=F("quantity") * (F("current_price") - F("buy_price")))
+    ).order_by("-updated_date")
+
+    errors = FhZeroUpTrend.objects.filter(
+        date=datetime.today(),
+        status__in=[FhZeroStatus.TO_BUY, FhZeroStatus.PURCHASED, FhZeroStatus.TO_SELL],
+        error=True,
+    ).annotate(profit_loss=F("quantity") * (F("sell_price") - F("buy_price")))
+
+    sold_data = FhZeroUpTrend.objects.filter(date=datetime.today(), status=FhZeroStatus.SOLD).annotate(
+        profit_loss=F("quantity") * (F("sell_price") - F("buy_price"))
+    )
+
+    context = {
+        "live_500": list(live_500.values()),
+        "live_pull_on": live_pull_on,
+        "live_polling_status": ps.status,
+        "progress": list(progress.values()),
+        "errors": list(errors.values()),
+        "purchased_data": list(sold_data.values()),
+        "realized_amount": sold_data.aggregate(Sum("profit_loss"))["profit_loss__sum"]
+    }
+
+    return render(request, "bengaluru/bengaluru_content.html", context=context)
