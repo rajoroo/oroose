7224b630025a44e7624484645487f6c76b389c32
maddy <madhavraj948@gmail.com>
2023-02-04 23:11:37 +0530

    commit 1

--- core/tools.py
+++ core/tools.py
@@ -1,4 +1,5 @@
 from core.models import ParameterConfig
+import numpy as np
 
 
 def get_param_config_tag(tag):
@@ -8,3 +9,38 @@ def get_param_config_tag(tag):
         return None
 
     return {rec.name: rec.content for rec in recs}
+
+
+def calculate_rsi(df):
+    """Calculate RSI dataframe has close price"""
+    df['change'] = df['close'].diff(1)  # Calculate change
+
+    # calculate gain / loss from every change
+    df['gain'] = np.select([df['change'] > 0, df['change'].isna()],
+                           [df['change'], np.nan],
+                           default=0)
+    df['loss'] = np.select([df['change'] < 0, df['change'].isna()],
+                           [-df['change'], np.nan],
+                           default=0)
+
+    # create avg_gain /  avg_loss columns with all nan
+    df['avg_gain'] = np.nan
+    df['avg_loss'] = np.nan
+
+    n = 14  # what is the window
+
+    # Alternatively
+    df['avg_gain'][n] = df.loc[:n, 'gain'].mean()
+    df['avg_loss'][n] = df.loc[:n, 'loss'].mean()
+
+    # This is not a pandas way, looping through the pandas series, but it does what you need
+    for i in range(n + 1, df.shape[0]):
+        df['avg_gain'].iloc[i] = (df['avg_gain'].iloc[i - 1] * (n - 1) + df['gain'].iloc[i]) / n
+        df['avg_loss'].iloc[i] = (df['avg_loss'].iloc[i - 1] * (n - 1) + df['loss'].iloc[i]) / n
+
+    # calculate rs and rsi
+    df['rs'] = df['avg_gain'] / df['avg_loss']
+    df['rsi'] = 100 - (100 / (1 + df['rs']))
+
+    # return df['rsi'].iloc[-3], df['rsi'].iloc[-2], df['rsi'].iloc[-1]
+    return df
--- stockwatch/models.py
+++ stockwatch/models.py
@@ -5,6 +5,8 @@ from django.db import models
 import pandas as pd
 from core.zero_util import get_history_five_min, get_kite, get_history_day
 from core.choice import FhZeroStatus, PlStatus
+from core.tools import calculate_rsi
+from dateutil.relativedelta import relativedelta
 
 
 from stockwatch.choice import SignalStatus
@@ -47,6 +49,13 @@ class FiveHundred(models.Model):
             models.UniqueConstraint(fields=["date", "symbol"], name="%(app_label)s_%(class)s_unique_five_hundred")
         ]
 
+    def get_date_diffetence(self):
+        to_date = datetime.today()
+        last_month_same_date = to_date - relativedelta(months=1, days=1)
+        exact_time = time(hour=9, minute=10)
+        from_date = datetime.combine(last_month_same_date, exact_time)
+        return from_date, to_date
+
     def is_valid_stock(self):
         symbol = self.symbol
         kite = get_kite()
@@ -85,96 +94,15 @@ class FiveHundred(models.Model):
 
         if current_list and len(current_list) > 16:
             df = pd.DataFrame({'close': current_list})
-            result_2, result_1, result = self.calculate_rsi(df=df)
+            result_df = calculate_rsi(df=df)
 
-            if (result_1 < 60 < result_2) and (result < 60 < result_2):
-                signal_status = SignalStatus.SELL
-            elif (result_1 > 60 > result_2) and (result > 60 > result_2):
-                signal_status = SignalStatus.BUY
-            print(result_2, result_1, result, signal_status, self.symbol)
+            result_2 = result_df['rsi'].iloc[-3]
+            result_1 = result_df['rsi'].iloc[-2]
+            result = result_df['rsi'].iloc[-1]
+
+            print(result_2, result_1, result, self.symbol)
             self.pp2 = result_2
             self.pp1 = result_1
             self.pp = result
             self.save()
         return signal_status
-
-    def calculate_rsi(self, df):
-        df['change'] = df['close'].diff(1)  # Calculate change
-
-        # calculate gain / loss from every change
-        df['gain'] = np.select([df['change'] > 0, df['change'].isna()],
-                               [df['change'], np.nan],
-                               default=0)
-        df['loss'] = np.select([df['change'] < 0, df['change'].isna()],
-                               [-df['change'], np.nan],
-                               default=0)
-
-        # create avg_gain /  avg_loss columns with all nan
-        df['avg_gain'] = np.nan
-        df['avg_loss'] = np.nan
-
-        n = 14  # what is the window
-
-        # Alternatively
-        df['avg_gain'][n] = df.loc[:n, 'gain'].mean()
-        df['avg_loss'][n] = df.loc[:n, 'loss'].mean()
-
-        # This is not a pandas way, looping through the pandas series, but it does what you need
-        for i in range(n + 1, df.shape[0]):
-            df['avg_gain'].iloc[i] = (df['avg_gain'].iloc[i - 1] * (n - 1) + df['gain'].iloc[i]) / n
-            df['avg_loss'].iloc[i] = (df['avg_loss'].iloc[i - 1] * (n - 1) + df['loss'].iloc[i]) / n
-
-        # calculate rs and rsi
-        df['rs'] = df['avg_gain'] / df['avg_loss']
-        df['rsi'] = 100 - (100 / (1 + df['rs']))
-
-        return df['rsi'].iloc[-3], df['rsi'].iloc[-2], df['rsi'].iloc[-1]
-
-    def calculate_macd(self, time_obj):
-        # from_date = datetime.today() - timedelta(days=60)
-        from_date = datetime(2022, 1, 20)
-        time_obj = datetime(2023, 1, 21)
-        df = get_history_day(
-            token=self.token,
-            from_date=from_date,
-            to_date=time_obj
-        )
-        k = df['close'].ewm(span=12, adjust=False, min_periods=12).mean()
-        d = df['close'].ewm(span=26, adjust=False, min_periods=26).mean()
-        macd = k - d
-        macd_s = macd.ewm(span=9, adjust=False, min_periods=9).mean()
-        macd_h = macd - macd_s
-        df['macd'] = df.index.map(macd)
-        df['macd_h'] = df.index.map(macd_h)
-        df['macd_s'] = df.index.map(macd_s)
-        signal = self.generate_signals(df)
-        df['buy_sig'] = signal[0]
-        df['sell_sig'] = signal[1]
-        print(df)
-        return df
-
-    def generate_signals(self, df):
-        buy_list = []
-        sell_list = []
-        flag = -1
-
-        for i in range(0, len(df)):
-            if df['macd'][i] > df['macd_s'][i]:  # first occurence of MACD crossing above signal oine
-                sell_list.append(np.nan)  # so first flip above means buy
-                if flag != 1:  # after first occurence I record flip to ignore
-                    buy_list.append(df['close'][i])  # from here onwards
-                    flag = 1
-                else:
-                    buy_list.append(np.nan)
-            elif df['macd'][i] < df['macd_s'][i]:
-                buy_list.append(np.nan)
-                if flag != 0:
-                    sell_list.append(df['close'][i])
-                    flag = 0
-                else:
-                    sell_list.append(np.nan)
-            else:
-                buy_list.append(np.nan)
-                sell_list.append(np.nan)
-
-        return (buy_list, sell_list)
