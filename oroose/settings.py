d09e84460793d10494491a599ad87181b0f3608d
maddy <madhavraj948@gmail.com>
2023-05-07 13:39:18 +0530

    commit 1

--- oroose/settings.py
+++ oroose/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
-    "django_celery_beat",
     "django_extensions",
     "core",
     "home",
