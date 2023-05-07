c20c4a8529ea8cb220fd9afed04c3ef5294e498a
maddy <madhavraj948@gmail.com>
2023-05-07 11:08:52 +0530

    commit 1

--- mysuru/views.py
+++ mysuru/views.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from django.db.models import Q
 from mysuru.models import TopTen, TrendStatus, MacdTrend
 from mysuru.polling_top_ten import polling_top_ten_stocks, trigger_calculate_top_ten
 from mysuru.polling_macd import polling_macd_stocks, trigger_calculate_macd
@@ -44,13 +44,33 @@ def mysuru_calculate_top_ten(request):
 # Uptrend
 @login_required(login_url="/accounts/login/")
 def macd_page(request):
-    macd_list = MacdTrend.objects.filter(trend_status=TrendStatus.YES, day_1_status=True)
+    day_1_list = MacdTrend.objects.filter(trend_status=TrendStatus.YES, day_1_status=True)
+    day_2_list = MacdTrend.objects.filter(trend_status=TrendStatus.YES, day_2_status=True)
+    valid_list = MacdTrend.objects.filter(Q(day_1_status=True, trend_status=TrendStatus.NO) | Q(day_2_status=True, trend_status=TrendStatus.NO))
+    invalid_list = MacdTrend.objects.filter(smart_token__isnull=True).count()
     to_calculate = MacdTrend.objects.filter(date=datetime.today(), ema_200__isnull=True).count()
+    macd_result = [
+        {
+            "title": "Day 1",
+            "macd_value": list(day_1_list.values()),
+            "macd_count": day_1_list.count(),
+        },
+        {
+            "title": "Day 2",
+            "macd_value": list(day_2_list.values()),
+            "macd_count": day_2_list.count(),
+        },
+        {
+            "title": "Valid",
+            "macd_value": list(valid_list.values()),
+            "macd_count": valid_list.count(),
+        },
+
+    ]
     context = {
         "active_page": "macd",
-        "macds": list(macd_list.values()),
-        "macd_count": macd_list.count(),
-        "to_calculate": to_calculate,
+        "macd_result": macd_result,
+        "to_calculate": to_calculate - invalid_list,
     }
     return render(request, "macd/base_page.html", context)
 
